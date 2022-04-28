ea47f67ee273ffca21c0acd77dadcdbc1463fb9e
Pranav <pranvofficial@gmail.com>
2022-04-28 17:28:13 +0530

    added deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,20 @@
-const main = async () => {
+async function main() {
   // ethers is referenced from the import at hardhat.config
   // HelloWorld is a factory function for creating contract instances
   const HelloWorld = await ethers.getContractFactory("HelloWorld");
 
   const hello_world = await HelloWorld.deploy("Hello World");
   console.log(`Smart contract deployed to address: ${hello_world.address}`);
-};
+}
 
 const callMain = async () => {
   try {
-    main();
+    await main();
     process.exit(0);
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
 };
+
+callMain();
